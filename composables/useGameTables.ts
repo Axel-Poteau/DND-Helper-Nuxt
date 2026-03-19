1b5f301d4fc8abb061561a9fcf165d0f98483150
axel <axelpoteau19@gmail.com>
2026-03-19 14:18:15 +0100

    fix game table and technical doc

--- composables/useGameTables.ts
+++ composables/useGameTables.ts
@@ -75,7 +75,7 @@ export function useGameTables() {
       .delete()
       .eq('id', id)
     if (err) { error.value = err.message; return }
-    await fetchOwnedTables()
+    await Promise.all([fetchOwnedTables(), fetchJoinedTables()])
   }
 
   async function joinTable(code: string, characterId: string): Promise<boolean> {
